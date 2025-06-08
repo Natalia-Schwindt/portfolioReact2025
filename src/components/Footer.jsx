341654642e47282b6510d702e4ad9dfc688d74ea
Natalia-Schwindt <naty8014@gmail.com>
2025-06-08 11:42:09 -0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import { VStack, Link } from "@chakra-ui/react"
+
+
+const Footer = () => {
+  return (
+    <VStack
+      as="footer">
+      <Link
+        href="#">Linkedin</Link>
+        <Link
+        href="#">Github</Link>
+        <Link
+        href="#">E-mail</Link>
+    </VStack>
+  )
+}
+
+export default Footer
