a0ae2f8220298858b5c5391201426133d6b6963d
Natalia-Schwindt <naty8014@gmail.com>
2025-06-08 22:16:39 -0300

    Mobile first

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,28 @@
-import { VStack, Link } from "@chakra-ui/react"
-
+import { Box, Flex, Link, Icon, Text, HStack, VStack } from "@chakra-ui/react";
+import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <VStack
-      as="footer">
-      <Link
-        href="#">Linkedin</Link>
-        <Link
-        href="#">Github</Link>
-        <Link
-        href="#">E-mail</Link>
-    </VStack>
-  )
-}
+    <Box as="footer" bg="transparent" color="white" py={6} mt={8}>
+      <VStack spacing={4}>
+        <HStack spacing={6}>
+          <Link href="https://www.linkedin.com/in/natalia-schwindt-3a3438115/" isExternal aria-label="LinkedIn">
+            <Icon as={FaLinkedin} boxSize={6} />
+          </Link>
+          <Link href="https://github.com/Natalia-Schwindt" isExternal aria-label="GitHub">
+            <Icon as={FaGithub} boxSize={6} />
+          </Link>
+          <Link href="mailto:naty8014l@gmail.com" aria-label="Correo">
+            <Icon as={FaEnvelope} boxSize={6} />
+          </Link>
+        </HStack>
 
-export default Footer
+        <Text fontSize="sm" textAlign="center">
+          Creado por Natalia Schwindt © 2025
+        </Text>
+      </VStack>
+    </Box>
+  );
+};
+
+export default Footer;
